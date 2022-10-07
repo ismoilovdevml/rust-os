d5a2d49d4e286771cb1191321e1bc6d38312728a
ismoilovdev <ismoilovdevarchlinux@gmail.com>
2022-10-07 20:13:17 +0500

    rust updated

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] 
+#![no_main] 
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
