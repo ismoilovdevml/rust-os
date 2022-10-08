680366e5c2fc4c3ede3a2bee4e08ff1139f03f77
ismoilovdev <ismoilovdevarchlinux@gmail.com>
2022-10-08 13:18:05 +0500

    rust kernel

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 #![no_std] 
 #![no_main]
-#![feature(asm)]
 
 use core::panic::PanicInfo;
 
+static SALOM: &[u8] = b"Hello Rust";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb800 as *mut u8;
+    for (i, &byte) in SALOM.iter().enumerate()  {
+        unsafe{
+            *vga_buffer.offset(i as isize *2) = byte;
+            *vga_buffer.offset(i as isize * 2+1) = 0xb;
+        }        
+    }
     loop {}
 }
 
