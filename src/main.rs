656ca6c9851291279678f11a276c7b1054ebec5a
ismoilovdev <ismoilovdevarchlinux@gmail.com>
2022-10-09 16:18:46 +0500

    VGA

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![no_std] 
+#![no_std]
 #![no_main]
 
 use core::panic::PanicInfo;
@@ -9,17 +9,12 @@ static SALOM: &[u8] = b"Hello Rust";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb800 as *mut u8;
-    for (i, &byte) in SALOM.iter().enumerate()  {
-        unsafe{
-            *vga_buffer.offset(i as isize *2) = byte;
-            *vga_buffer.offset(i as isize * 2+1) = 0xb;
-        }        
-    }
+    println!("Birinchi Kernel{}", "!");
     loop {}
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
