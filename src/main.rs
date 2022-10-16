1cae917d5933cdb3a8280fd7721c5ee200cb0aef
ismoilovdev <ismoilovdevarchlinux@gmail.com>
2022-10-17 00:39:07 +0500

    update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(rust_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
+use rust_os::println;
 use core::panic::PanicInfo;
 
 mod vga_buffer;
@@ -8,11 +12,26 @@ mod vga_buffer;
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Birinchi Kernel{}", "!");
+
+    #[cfg(test)]
+    test_main();
     loop {}
 }
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn  panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn trivial_assertion(){
+    assert_eq!(1, 1);
 }
